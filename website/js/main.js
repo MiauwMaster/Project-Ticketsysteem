2268025a0d138e31a36c6237aa284af54ce52f19
AltijdMelvin <bos12bos@hotmail.com>
2017-04-05 14:55:39 +0200

    Nieuwe updates layout
    Voornamelijk ticketbestellen

--- website/js/main.js
+++ website/js/main.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
     $(".dropdown-toggle").dropdown();
 });
 
+$(document).on('click', '.value-control', function () {
+    var action = $(this).attr('data-action');
+    var target = $(this).attr('data-target');
+    var value = parseFloat($('[id="' + target + '"]').val());
+    if (action === "plus") {
+        value++;
+    }
+    if (action === "minus") {
+        if (value === 0) {
+            $button.addClass('inactive');
+        }
+        else {
+            value--;
+        }
+    }
+    $('[id="' + target + '"]').val(value);
+});
+
 function initMap() {
     var uluru = { lat: -25.363, lng: 131.044 };
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -13,3 +31,4 @@ function initMap() {
         map: map
     });
 }
+
